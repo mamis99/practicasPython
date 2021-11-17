597ead8629a4f8a791a3ee976dfa39af2136f974
mamis99 <isaacmar24@gmail.com>
2021-11-17 15:15:35 -0600

    La primer tarea realizada

---
+++ dia2/e2.py
@@ -0,0 +1,13 @@
+year = input("Ingresa el año: ")
+try:
+    int(year)
+    check = True
+except ValueError:
+    check = False
+if check == True:
+    if int(year) % 4 == 0 and int(year) % 100 != 0 or int(year) % 400 == 0:
+        print(f"{year} es un año bisiesto")
+    else:
+        print(f"{year} no es un año bisiesto")
+else:
+    print("No ingresaste un año")
